8354ff49a62246cd6e7a9b975901ed5da6710b96
Zeetbor <51408938+Zeetbor@users.noreply.github.com>
2020-06-01 19:28:41 +0100

    Fixed calculation logic
    Fixed calculation logic so results of operation are displayed properly on the screen

--- calculator.js
+++ calculator.js
@@ -1,7 +1,25 @@
-var inputa= "";
-var inputb = "";
+var input = "";
+var memory = "";
 var output = "";
 var operator = "";
+var scrn = "";
+
+const input_div = document.querySelector(".input");
+const output_div = document.querySelector(".output");
+
+display = (num) => {
+  scrn += (num);
+  return input_div.innerHTML = scrn;
+}
+
+clear = () => {
+  input = '';
+  memory = '';
+  output = '';
+  scrn = '';
+  input_div.innerHTML = '';
+  output_div.innerHTML = '';
+}
 
 /*
 const num_buttons = document.querySelectorAll(".number");
@@ -14,89 +32,114 @@ function numberPress(e){
 }
 */
 
+// NUMBER BUTTONS ------------------------------------
+
 const one = document.getElementById("one");
 one.addEventListener('click', () => {
+  input += (1);
   display(1);
 });
 
 const two = document.getElementById("two");
 two.addEventListener('click', () => {
+  input += (2);
   display(2);
 });
 
 const three = document.getElementById("three");
 three.addEventListener('click', () => {
+  input += (3);
   display(3);
 });
 
 const four = document.getElementById("four");
 four.addEventListener('click', () => {
+  input += (4);
   display(4);
 });
 
 const five = document.getElementById("five");
 five.addEventListener('click', () => {
+  input += (5);
   display(5);
 });
 
 const six = document.getElementById("six");
 six.addEventListener('click', () => {
+  input += (6);
   display(6);
 });
 
 const seven = document.getElementById("seven");
 seven.addEventListener('click', () => {
+  input += (7);
   display(7);
 });
 
 const eight = document.getElementById("eight");
 eight.addEventListener('click', () => {
+  input += (8);
   display(8);
 });
 
 const nine = document.getElementById("nine");
 nine.addEventListener('click', () => {
+  input += (9);
   display(9);
 });
 
 const zero = document.getElementById("zero");
 zero.addEventListener('click', () => {
+  input += (0);
   display(0);
 });
 
 
 
+// OPERATION BUTTONS ----------------------------------
+
 const divide_button = document.getElementById("divide");
 divide_button.addEventListener('click', () => {
   display("/");
+  operator = "/";
+  memory = input;
+  input = '';
 });
 
 const multiply_button = document.getElementById("multiply");
 multiply_button.addEventListener('click', () => {
-  display("*");
+  display("x");
+  operator = "*";
+  memory = input;
+  input = '';
 });
 
 const subtract_button = document.getElementById("subtract");
 subtract_button.addEventListener('click', () => {
   display("-");
-  if (operator = "") {
-    operator += "-";
-    inputa == inputb;
-    output == inputb;
-    return output_div.innerHTML = operate(inputa, operator, inputb);
-  }
+  operator = "-";
+  memory = input;
+  input = '';
 });
 
 const add_button = document.getElementById("add");
 add_button.addEventListener('click', () => {
   display("+");
+  operator = "+";
+  memory = input;
+  input = '';
 });
-
 
 const equals_button = document.getElementById("equals");
 equals_button.addEventListener('click', () => {
-  operate(inputa, inputb);
-  return output_div.innerHTML = operate(inputa);
+  output = operate(memory, operator, input);
+  return output_div.innerHTML = output;
+  /*if (operator = "") {
+    operator += "-";
+    input == memory;
+    output == memory;
+    return output_div.innerHTML = operate(input, operator, memory);
+  }*/
 });
 
 const clear_button = document.getElementById("clear");
@@ -104,22 +147,8 @@ clear_button.addEventListener('click', () => {
   clear();
 });
 
-const input_div = document.querySelector(".input");
-const output_div = document.querySelector(".output");
 
-display = (num) => {
-  inputa += num;
-  return input_div.innerHTML = inputa;
-}
-
-clear = () => {
-  inputa = '';
-  inputb = '';
-  output = '';
-  input_div.innerHTML = '';
-  output_div.innerHTML = '';
-}
-
+// OERATIONS --------------------------------------
 
 add = (n1, n2) =>  {
  return Number(n1) + Number(n2);
@@ -157,3 +186,27 @@ operate = (n1, op, n2) => {
       break;
   }
 }
+
+/*
+
+button logic:
+on number press > input++
+
+on operator press > if memory = "" then
+                    input = memory
+                    & input = ""
+                    else
+                    memory (+-/*) input = memory
+                    input = ""
+*/
+
+/*
+
+Double operator logic:
+
+If Input.length is NaN
+input - last value
+display (op)
+scrn + optimize
+
+*/
